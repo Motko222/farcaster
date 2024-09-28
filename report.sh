b118b31d042498299672427fae5fcfe5e18c588b
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-28 21:11:23 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+json=~/logs/report-$folder
 source ~/.bash_profile
-
-id=$FARCASTER_ID
-chain=mainnet
-network=mainnet
-type="hubble"
-group=node
-owner=$OWNER
 
 docker_status=$(docker inspect hubble_hubble_1 | jq -r .[].State.Status)
 version=$(docker logs hubble_hubble_1 2>&1 | grep -a "Hubble: " | tail -1 | awk -F "Hubble: " '{print $NF}'| awk -F '\\\\n' '{print $1}')
@@ -19,26 +15,29 @@ esac
 
 cat << EOF
 {
-  "id":"$id",
-  "machine":"$MACHINE",
-  "version":"$version",
-  "chain":"$chain",
-  "network":"$network",
-  "status":"$status",
-  "message":"$message",
-  "updated":"$(date --utc +%FT%TZ)"
+
 }
 EOF
 
-# send data to influxdb
-if [ ! -z $INFLUX_HOST ]
-then
- curl --request POST \
- "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$INFLUX_BUCKET&precision=ns" \
-  --header "Authorization: Token $INFLUX_TOKEN" \
-  --header "Content-Type: text/plain; charset=utf-8" \
-  --header "Accept: application/json" \
-  --data-binary "
-    report,id=$id,machine=$MACHINE,grp=$group,owner=$owner status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\",network=\"$network\" $(date +%s%N) 
-    "
-fi
+cat >$json << EOF
+{ 
+  "updated":"$(date --utc +%FT%TZ)",
+  "measurement":"report",
+  "tags": {
+      "id":"$folder",
+      "machine":"$MACHINE",
+      "owner":"$OWNER",
+      "grp":"node" 
+  },
+  "fields": {
+      "version":"$version",
+      "chain":"mainnet",
+      "network":"mainnet",
+      "status":"$status",
+      "message":"$message",
+      "docker_status":"$docker_status"
+  }
+}
+EOF
+
+cat $json | jq
